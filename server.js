1f4fee44764a4294b9b4c38efda00abbd4083153
Ujjwal Singh <ujjwalsingh6386@gmail.com>
2022-09-08 18:33:45 +0530

    added cart page and auth

--- server.js
+++ server.js
@@ -12,9 +12,7 @@ app.use(express.static('public'))
 
 
 
-app.get('/',(req,res)=>{
-    res.render("home")
-})
+
 
 //set template engine
 app.use(expressLayout)
@@ -23,7 +21,20 @@ app.set('views',path.join(__dirname, '/resources/views'))
 app.set('view engine','ejs')
 
 
+app.get('/',(req,res)=>{
+    res.render("home")
+})
 
+app.get("/cart",(req,res)=>{
+    res.render("customers/cart")
+})
+
+app.get("/login",(req,res)=>{
+    res.render("auth/login")
+})
+app.get("/register",(req,res)=>{
+    res.render("auth/register")
+})
 
 
 
